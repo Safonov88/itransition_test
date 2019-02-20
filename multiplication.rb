d72dbf2e27f908e94fe926d46fddc15ed1a0fe38
Дмитрий <safonovdmitr@gmail.com>
2019-02-20 14:39:43 +0300

    create method max_multiplication

--- multiplication.rb
+++ multiplication.rb
@@ -1,6 +1,19 @@
 module Multiplication
+  extend self
+
   def max_multiplication(string)
-    return nil unless string?(string) || combinations?(string)
+    return nil unless string?(string)
+    combinations = combinations(string)
+    return nil if combinations.empty?
+
+    mulitiplication_array = []
+    combinations.map do |combination|
+      sub_combination = []
+      combination.each_char { |s| sub_combination << s.to_i }
+      mulitiplication_array << (sub_combination[0] * sub_combination[1] * sub_combination[2] * sub_combination[3])
+    end
+
+    mulitiplication_array.sort.last
   end
 
   def string?(string)
@@ -21,9 +34,5 @@ module Multiplication
 
   def correct_combination?(string)
     string.scan(/\D/).empty?
-  end
-
-  def combinations?(string)
-    !combinations(string).empty?
   end
 end
