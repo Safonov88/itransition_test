043cc66e3b5cad0b53b2f80885f38efc4d4b9521
Дмитрий <safonovdmitr@gmail.com>
2019-02-20 16:32:04 +0300

    fix sort_spec

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -1,8 +1,10 @@
 require 'rspec'
 require_relative '../sort'
 
-describe 'Sort' do
-  it 'returns correct sort array' do
-    expect(Sort::sort_by_binary([3,7,8,9])).to eq([8,3,9,7])
+describe 'module Sort' do
+  describe '#sort_by_binary' do
+    it 'returns correct sort array' do
+      expect(Sort::sort_by_binary([3,7,8,9])).to eq([8,3,9,7])
+    end
   end
 end
