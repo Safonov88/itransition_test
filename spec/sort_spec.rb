427e2f7de7a483c9d76567173b03022e902cbe8f
Дмитрий <safonovdmitr@gmail.com>
2019-02-20 18:13:43 +0300

    create test sorts array when value is negative

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -3,8 +3,16 @@ require_relative '../sort'
 
 describe 'module Sort' do
   describe '#sort_by_binary' do
-    it 'sorts array' do
-      expect(Sort.sort_by_binary([3,7,8,9])).to eq([8,3,9,7])
+    context 'when all value is positive' do
+      it 'sorts array' do
+        expect(Sort.sort_by_binary([3,7,8,9])).to eq([8,3,9,7])
+      end
+    end
+
+    context 'when value is negative' do
+      it 'sorts array' do
+        expect(Sort.sort_by_binary([-3,7,-8,9])).to eq([-8,-3,9,7])
+      end
     end
   end
 end
