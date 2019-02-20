0d4cab551c2a662930f700a97414dde3e2c941ce
Дмитрий <safonovdmitr@gmail.com>
2019-02-20 18:07:55 +0300

    fix codestyle

--- spec/multiplication_spec.rb
+++ spec/multiplication_spec.rb
@@ -5,13 +5,13 @@ describe 'module Multiplication' do
   describe '#string?' do
     context 'when value is string' do
       it 'returns true' do
-        expect(Multiplication::string?('test_string')).to be_truthy
+        expect(Multiplication.string?('test_string')).to be_truthy
       end
     end
 
     context 'when value is not string' do
       it 'returns false' do
-        expect(Multiplication::string?(2584)).to be_falsey
+        expect(Multiplication.string?(2584)).to be_falsey
       end
     end
   end
@@ -19,13 +19,13 @@ describe 'module Multiplication' do
   describe '#correct_combination?' do
     context 'when the value include only numbers' do
       it 'returns true' do
-        expect(Multiplication::correct_combination?('2548')).to be_truthy
+        expect(Multiplication.correct_combination?('2548')).to be_truthy
       end
     end
 
     context 'when the value includes letters' do
       it 'returns false' do
-        expect(Multiplication::correct_combination?('2a48')).to be_falsey
+        expect(Multiplication.correct_combination?('2a48')).to be_falsey
       end
     end
   end
@@ -33,26 +33,26 @@ describe 'module Multiplication' do
   describe '#combinations' do
     context 'when string include valid combinations' do
       it 'returns an array with values containing only digits' do
-        expect(Multiplication::combinations('abc12345def')).to eq(['1234', '2345'])
+        expect(Multiplication.combinations('abc12345def')).to eq(['1234', '2345'])
       end
     end
 
     context 'when string include invalid combinations' do
       it 'returns empty array' do
-        expect(Multiplication::combinations('abcd12ef345')).to eq([])
+        expect(Multiplication.combinations('abcd12ef345')).to eq([])
       end
     end
   end
 
   describe '#multiplication' do
     it 'checks correct multiplication' do
-      expect(Multiplication::multiplication(['1234', '5678'])).to eq([24, 1680])
+      expect(Multiplication.multiplication(['1234', '5678'])).to eq([24, 1680])
     end
   end
 
   describe '#max_multiplication' do
     it 'returns max multiplication value' do
-      expect(Multiplication::max_multiplication('abc12345def')).to eq(120)
+      expect(Multiplication.max_multiplication('abc12345def')).to eq(120)
     end
   end
 end
--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -3,8 +3,8 @@ require_relative '../sort'
 
 describe 'module Sort' do
   describe '#sort_by_binary' do
-    it 'returns correct sort array' do
-      expect(Sort::sort_by_binary([3,7,8,9])).to eq([8,3,9,7])
+    it 'sorts array' do
+      expect(Sort.sort_by_binary([3,7,8,9])).to eq([8,3,9,7])
     end
   end
 end
