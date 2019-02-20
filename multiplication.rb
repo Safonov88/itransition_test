6c41abeff3e4fc697db9c03c4f2ac4ac14d15a86
Дмитрий <safonovdmitr@gmail.com>
2019-02-20 12:37:55 +0300

    create method string?

--- multiplication.rb
+++ multiplication.rb
@@ -1,2 +1,9 @@
 module Multiplication
+  def max_multiplication(string)
+    return nil if string?(string) || combinations?(string)
+  end
+
+  def string?(string)
+    string.class == String
+  end
 end
