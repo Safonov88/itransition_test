b89b9d4693955e4e791206264d59625cb24da7d6
Дмитрий <safonovdmitr@gmail.com>
2019-02-20 13:02:35 +0300

    create method combinations

--- multiplication.rb
+++ multiplication.rb
@@ -1,9 +1,19 @@
 module Multiplication
   def max_multiplication(string)
-    return nil if string?(string) || combinations?(string)
+    return nil unless string?(string) || combinations?(string)
   end
 
   def string?(string)
     string.class == String
+  end
+
+  def combinations(string)
+    array = []
+    count_sub_strings = string.size - 3
+
+    count_sub_strings.times do |first|
+      last = first + 3
+      array << string[first..last]
+    end
   end
 end
